1ccc5e5a392a3d68bda1bc48f8d6f1d80bcd001e
cuwwee <smb.amanzhol@gmail.com>
2026-02-07 10:58:29 +0500

    Boolean_intro

---
+++ Practice2/boolean/boolean_intro.py
@@ -0,0 +1,16 @@
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+# execute code based on the Boolean answer of a function:
+  def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
