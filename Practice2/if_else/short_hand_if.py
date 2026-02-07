004be82e10e461bad25a74fc7b46f9e6e05c0c00
cuwwee <smb.amanzhol@gmail.com>
2026-02-07 11:10:55 +0500

    short hand if

---
+++ Practice2/if_else/short_hand_if.py
@@ -0,0 +1,14 @@
+# shorthand  if
+a = 5
+b = 2
+if a > b: print("a is greater than b")
+
+#shorthand else if
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+# multiple conditions
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
