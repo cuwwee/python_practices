58221a7ffdc585f4b2736bf20712aa4fcc9faa59
cuwwee <smb.amanzhol@gmail.com>
2026-02-07 11:02:58 +0500

    Different boolean operators

---
+++ Practice2/boolean/boolean_operators.py
@@ -0,0 +1,27 @@
+# arithmetic operators
+x = 15
+y = 4
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+# identity operators
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is z)
+print(x is y)
+print(x == y)
+
+# membership
+text = "Hello World"
+
+print("H" in text)
+print("hello" in text)
+print("z" not in text)
